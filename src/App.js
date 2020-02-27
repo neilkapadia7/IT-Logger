da318382e5d2f351fe0e1433888c05ba5e8bfdaa
neilkapadia7 <neilkapadia7@gmail.com>
2020-02-27 14:15:55 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, Fragment} from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
-function App() {
+
+const App = () => {
   useEffect(() => {
     // Initializes Materialize JS
     M.AutoInit();
-  })
+  });
+
   return (
-    <div className="App">
-      My App
-    </div>
+    <Fragment>
+      <SearchBar />
+      <Logs />
+    </Fragment>
   );
 }
 
