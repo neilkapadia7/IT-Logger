b0c520e211a8e50389747432c11da1e8fb501acb
neilkapadia7 <neilkapadia7@gmail.com>
2020-02-29 16:26:52 +0530

    Update Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, {useEffect, Fragment} from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal'
+import TechListModal from './components/techs/TechListModal';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
   useEffect(() => {
@@ -13,10 +21,19 @@ const App = () => {
   });
 
   return (
+    <Provider store={store}>
     <Fragment>
       <SearchBar />
-      <Logs />
+      <div className='container'>
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <Logs />
+        <TechListModal />
+      </div>
     </Fragment>
+    </Provider>
   );
 }
 
--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Moment from 'react-moment';
+import {connect} from 'react-redux';
+import {deleteLog, setCurrent} from '../../actions/logActions';
 import PropTypes from 'prop-types';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
-const LogItem = ({ log }) => {
+const LogItem = ({ log, deleteLog, setCurrent }) => {
+
+    const onDelete = () => {
+        deleteLog(log.id);
+        M.toast({ html: 'Log Deleted'});
+    }
+
+
     return (
         <li className='collection-item'>
             <div>
-                <a href='#edit-log-modal' className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}>
+                <a href='#edit-log-modal' onClick={() => setCurrent(log)} className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}>
                     {log.message}
                 </a>
                 <br />
@@ -18,7 +28,7 @@ const LogItem = ({ log }) => {
                 </span>
 
                 <a className='secondary-content' href='#!'>
-                    <i className='material-icons grey-text'>delete</i>
+                    <i className='material-icons grey-text' onClick={onDelete}>delete</i>
                 </a>
             </div>
         </li>
@@ -26,7 +36,9 @@ const LogItem = ({ log }) => {
 }
 
 LogItem.propTypes = {
-    log: PropTypes.object.isRequired
+    log: PropTypes.object.isRequired,
+    deleteLog: PropTypes.func.isRequired,
+    setCurrent: PropTypes.func.isRequired
 }
 
-export default LogItem;
+export default connect(null, {deleteLog, setCurrent})(LogItem);
